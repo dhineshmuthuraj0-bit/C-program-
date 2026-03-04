c4b51622b74ba0488b25a5753a490b9c2673ff7c
Dhinesh M <dhineshmuthuraj0@gmail.com>
2026-03-04 15:57:46 +0530

    Create C arr15.c

---
+++ C arr15.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j = 0;
+    scanf("%d", &n);
+    int arr[n];
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    // Move non-zero elements forward
+    for(i = 0; i < n; i++)
+        if(arr[i] != 0)
+            arr[j++] = arr[i];
+
+    // Fill remaining with zero
+    while(j < n)
+        arr[j++] = 0;
+
+    for(i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
