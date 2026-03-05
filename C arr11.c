d3e87d063242b58015ddbe211280f1c9dfc2bfdb
Dhinesh M <dhineshmuthuraj0@gmail.com>
2026-03-05 11:38:47 +0530

    Create C arr11.c

--- C arr11.c
+++ C arr11.c
@@ -3,28 +3,20 @@
 int main() {
     int n, i;
     scanf("%d", &n);
-
     int arr[n];
     int total = 0, left = 0;
-
-    // Read expenses and calculate total sum
     for(i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
         total += arr[i];
     }
-
-    // Find equilibrium index
     for(i = 0; i < n; i++) {
-        total -= arr[i];   // Right sum
-
+        total -= arr[i]; 
         if(left == total) {
             printf("%d", i);
             return 0;
         }
-
         left += arr[i];
     }
-
     printf("-1");
     return 0;
 }
