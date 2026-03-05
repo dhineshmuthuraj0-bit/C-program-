8ec4e5daced8c16212d1264d8907a883de37e151
Dhinesh M <dhineshmuthuraj0@gmail.com>
2026-03-05 11:46:18 +0530

    Create C arr6.c

--- C arr6.c
+++ C arr6.c
@@ -2,35 +2,22 @@
 
 int main() {
     int N, i, j;
-    
-    // Read number of customer IDs
     scanf("%d", &N);
-    
-    int arr[N];  // Array to store customer IDs
-    
-    // Read customer IDs
+    int arr[N]; 
     for(i = 0; i < N; i++) {
         scanf("%d", &arr[i]);
     }
-    
-    // Loop through each element
     for(i = 0; i < N; i++) {
-        
-        int isDuplicate = 0;  // Flag to check if element appeared before
-        
-        // Check previous elements
+        int isDuplicate = 0; 
         for(j = 0; j < i; j++) {
             if(arr[i] == arr[j]) {
-                isDuplicate = 1;  // Mark as duplicate
+                isDuplicate = 1;  
                 break;
             }
-        }
-        
-        // If not duplicate, print the element
+         }
         if(isDuplicate == 0) {
             printf("%d ", arr[i]);
         }
     }
-    
     return 0;
 }
