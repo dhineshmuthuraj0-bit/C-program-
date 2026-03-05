0c27afbf64c0836c23ee975a0b15e37425752425
Dhinesh M <dhineshmuthuraj0@gmail.com>
2026-03-05 11:50:51 +0530

    Create C arr7.c

--- C arr7.c
+++ C arr7.c
@@ -1,31 +1,21 @@
 #include <stdio.h>
 
-int main() 
-{
+int main() {
     int n, i, num;
     int evenCount = 0, oddCount = 0;
-
-    // Read the number of days
     scanf("%d", &n);
-
-    // Loop to read each day number
     for(i = 0; i < n; i++)
     {
         scanf("%d", &num);
-
-        // Check if the number is even
         if(num % 2 == 0)
         {
-            evenCount++;   // Increment even counter
+            evenCount++;   
         }
         else
         {
-            oddCount++;    // Increment odd counter
+            oddCount++; 
         }
     }
-
-    // Print the result
     printf("Even:%d Odd:%d", evenCount, oddCount);
-
     return 0;
 }
