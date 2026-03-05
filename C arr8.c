4c26ff0adc65bb4bc0b1c553131887eb86adfead
Dhinesh M <dhineshmuthuraj0@gmail.com>
2026-03-05 11:49:51 +0530

    Create C arr8.c

--- C arr8.c
+++ C arr8.c
@@ -1,33 +1,22 @@
 #include <stdio.h>
 
-int main()
-{
+int main(){
     int N, K, i;
-
-    // Read number of elements
     scanf("%d", &N);
-
     int arr[N];
-
-    // Read array elements
     for(i = 0; i < N; i++)
     {
         scanf("%d", &arr[i]);
     }
-
-    // Read value of K
     scanf("%d", &K);
-
-    // Print rotated array (Left rotation by K)
     for(i = K; i < N; i++)
     {
-        printf("%d ", arr[i]);   // Print elements from K to N-1
+        printf("%d ", arr[i]); 
     }
 
     for(i = 0; i < K; i++)
     {
-        printf("%d ", arr[i]);   // Print first K elements at the end
+        printf("%d ", arr[i]);   /
     }
-
     return 0;
 }
