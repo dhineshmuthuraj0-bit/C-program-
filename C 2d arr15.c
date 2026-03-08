e813d2b8a62b4c3af03922fc341564794a28fb4b
Dhinesh M <dhineshmuthuraj0@gmail.com>
2026-03-08 22:32:12 +0530

    Create C 2d arr15.c

---
+++ C 2d arr15.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int isPrime(int n){
+    if(n<2) return 0;
+    for(int i=2;i*i<=n;i++)
+        if(n%i==0) return 0;
+    return 1;
+}
+int main(){
+    int R,C,i,j,count=0;
+    scanf("%d%d",&R,&C);
+    int a[R][C];
+    for(i=0;i<R;i++)
+        for(j=0;j<C;j++)
+            scanf("%d",&a[i][j]);
+    for(i=0;i<R;i++)
+        for(j=0;j<C;j++)
+            if(isPrime(a[i][j])) count++;
+    printf("%d",count);
+}
