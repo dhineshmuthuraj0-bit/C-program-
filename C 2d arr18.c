732123a5e1938ca6aaeb238cf5f617e522512579
Dhinesh M <dhineshmuthuraj0@gmail.com>
2026-03-08 22:09:05 +0530

    Create C 2d arr18.c

---
+++ C 2d arr18.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int R,C,i,j,k,l,max=0;
+    scanf("%d%d",&R,&C);
+    int a[R][C];
+    for(i=0;i<R;i++)
+        for(j=0;j<C;j++)
+            scanf("%d",&a[i][j]);
+    for(i=0;i<R;i++)
+        for(j=0;j<C;j++)
+            for(k=i+1;k<R;k++)
+                for(l=0;l<C;l++)
+                    if(abs(a[i][j]-a[k][l])>max)
+                        max=abs(a[i][j]-a[k][l]);
+    printf("%d",max);
+}
